22c11898a3c38bf48dbcd3d4f81d29fbb5f1bc83
Brandon Weaver <a7xncob@gmail.com>
2020-01-30 19:17:45 +0000

    Automatically backed up by Learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,7 @@
 module SongsHelper
-
+  
+  def display_artist(song)
+    return song.artist ? link_to song.artist.name, artist_path(song.artist) : link_to song.title, edit_song_path(song)
+  end
+  
 end
